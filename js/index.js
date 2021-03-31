8922764994c6e5189b899ec606f89dd9420b1a4e
Joao Vitor Soares <joaovitorsoares12@gmail.com>
2021-03-31 12:55:03 -0300

    Corrigindo as msgs no whats

--- js/index.js
+++ js/index.js
@@ -1,25 +1,13 @@
 var linkWhats = "https://api.whatsapp.com/send?phone=556381590358&text=";
-var turno = ['bom dia!', 'boa tarde!', 'boa noite!'];
 
-function msg(turno, tema) {
+function msg(tema) {
     var textUppercase = tema.toUpperCase();
-    return `Olá, ${turno} Poderia me ajudar? Gostaria de falar sobre ${textUppercase}`
+    return `Olá, poderia me ajudar? Gostaria de falar sobre ${textUppercase}`
 }
 
 function enviarMsg(value) {
-    var date = new Date()
-    var hour = date.getHours();
+    var msgpadrao = msg(value);
+    window.open(linkWhats + msgpadrao);
 
-    if (hour>=6 && hour < 12) {
-        var msgpadrao = msg(turno[0], value);
-        window.open(linkWhats + msgpadrao);
-    } else if (hour > 12 && hour < 18) {
-        var msgpadrao = msg(turno[1], value);
-        window.open(linkWhats + msgpadrao);
-    } else {
-        var msgpadrao = msg(turno[2], value);
-        window.open(linkWhats + msgpadrao);
-    }
-    //window.open(linkWhats + value)
 }
 
